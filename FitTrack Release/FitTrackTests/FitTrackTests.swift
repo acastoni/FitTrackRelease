8213b5444ebffdb2ccc7c1c22471ee3db3e74d39
Anthony Castillo <acast698@fiu.edu>
2024-07-19 14:52:12 -0400

    Add files via upload
    Full App

---
+++ FitTrack Release/FitTrackTests/FitTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitTrackTests.swift
+//  FitTrackTests
+//
+//  Created by Anthony Castillo on 7/16/24.
+//
+
+import Testing
+@testable import FitTrack
+
+struct FitTrackTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
